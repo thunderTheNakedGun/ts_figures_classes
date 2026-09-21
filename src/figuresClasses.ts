0e8b992e6363b539ba449fd2a6293aefe782d385
Balazs Radvanyi <balazs@riltech.co>
2026-09-21 12:15:09 +0200

    feat: implement Triangle, Circle, and Rectangle figure classes

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,11 +1,79 @@
-export interface Figure {}
+export type Shape = 'triangle' | 'circle' | 'rectangle';
+export type Color = 'red' | 'green' | 'blue';
 
-export class Triangle implements Figure {}
+export interface Figure {
+  shape: Shape;
+  color: Color;
 
-export class Circle implements Figure {}
+  getArea: () => number;
+}
 
-export class Rectangle implements Figure {}
+export class Triangle implements Figure {
+  constructor(
+    public color: Color,
+    public a: number,
+    public b: number,
+    public c: number,
+    public shape: Shape = 'triangle',
+  ) {
+    if ([a, b, c].some((side) => side <= 0)) {
+      throw new Error('triangle sides should be positive!');
+    }
 
-export function getInfo(figure): string {
-  return typeof figure;
+    if (a >= b + c || b >= a + c || c >= a + b) {
+      throw new Error(
+        'the longest side should not be greater than the other two sides!',
+      );
+    }
+  }
+
+  getArea = (): number => {
+    const semiPerimeter = (this.a + this.b + this.c) / 2;
+
+    // Apply Heron's formula: A = √(s(s-a)(s-b)(s-c))
+    const area = Math.sqrt(
+      semiPerimeter *
+        (semiPerimeter - this.a) *
+        (semiPerimeter - this.b) *
+        (semiPerimeter - this.c),
+    );
+
+    return +area.toFixed(2);
+  };
+}
+
+export class Circle implements Figure {
+  shape: Shape = 'circle';
+
+  constructor(
+    public color: Color,
+    public radius: number,
+  ) {
+    if (radius <= 0) {
+      throw new Error('circle radius should be positive!');
+    }
+  }
+
+  getArea = (): number =>
+    Math.floor(Math.PI * this.radius * this.radius * 100) / 100;
+}
+
+export class Rectangle implements Figure {
+  shape: Shape = 'rectangle';
+
+  constructor(
+    public color: Color,
+    public width: number,
+    public height: number,
+  ) {
+    if (width <= 0 || height <= 0) {
+      throw new Error('rectangle sides should be positive!');
+    }
+  }
+
+  getArea = (): number => +(this.width * this.height).toFixed(2);
+}
+
+export function getInfo(figure: Figure): string {
+  return `A ${figure.color} ${figure.shape} - ${figure.getArea()}`;
 }
